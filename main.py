f4bf5ed8a0bbfc9f5b12cd8d349b8f7d8adbea4d
Austin <austin.r.mandy@gmail.com>
2026-02-18 22:49:34 -0800

    Added footers and proper amount of pages per topic via the csv file info

--- main.py
+++ main.py
@@ -2,14 +2,31 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
     pdf.add_page()
+    
+    #Header
     pdf.set_font(family="Times", style="B", size=24)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10,21,200,21)
+    
+    pdf.ln(258)
+    
+    #Footer
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+    
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
+        pdf.ln(270)
+        
+        #Footer for other pages per Topic
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
 
 
